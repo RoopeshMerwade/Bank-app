e9a17388519a22f941e00f730313445ddc5c04ae
Roopesh Merwade <roopesh.merwade20@gmail.com>
2024-01-30 20:01:09 +0530

    modified

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "INR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,6 +29,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "INR",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -24,6 +48,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    "2020-03-15T08:30:45.123Z",
+    "2020-05-20T11:55:27.689Z",
+    "2020-08-07T16:22:18.543Z",
+    "2020-09-12T09:37:50.826Z",
+    "2020-10-28T14:05:32.977Z",
+    "2020-12-03T17:20:54.814Z",
+    "2020-12-20T13:45:11.632Z",
+    "2020-12-31T22:59:59.999Z",
+  ],
+  currency: "INR",
+  locale: "en-US",
 };
 
 const account4 = {
@@ -31,6 +67,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    "2020-02-10T10:30:15.456Z",
+    "2020-03-22T15:40:37.892Z",
+    "2020-05-05T18:55:49.721Z",
+    "2020-07-08T08:12:04.633Z",
+    "2020-08-14T12:26:29.475Z",
+    "2020-09-30T16:37:55.588Z",
+    "2020-11-07T20:45:12.374Z",
+    "2020-12-15T09:01:30.912Z",
+  ],
+  currency: "INR",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -49,6 +97,7 @@ const namewish = document.querySelector(".Day");
 const transferto = document.querySelector("#input1");
 const amounttotransfer = document.querySelector("#input2");
 const transferbtn = document.querySelector(".transferbtn");
+const transactiondate = document.querySelector(".transaction-date");
 
 // close-account
 const closeacctbtn = document.querySelector(".close-btn");
@@ -330,3 +379,17 @@ sortbtn.addEventListener("click", function (e) {
 
   sorted = !sorted;
 });
+
+function TransactionDates(account) {
+  const formattedDates = account.movementsDates.map((datestring) => {
+    const originalDate = new Date(datestring);
+    const day = String(originalDate.getUTCDate()).padStart(2, "0");
+    const month = String(originalDate.getUTCMonth() + 1).padStart(2, 0);
+    const year = originalDate.getUTCFullYear();
+
+    return `${day}/${month}/${year}`;
+  });
+  return formattedDates;
+}
+// Rough
+console.log(TransactionDates(account1.movementsDates));
